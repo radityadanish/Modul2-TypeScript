aa37fdb6ae18d2dc9e1f52038fb6658f77d2cc11
radityadanish <radityadanish321@gmail.com>
2026-04-15 09:35:44 +0700

    index.js

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function login(username, password) {
     return username === "admin" && password === "admin";
 }
 console.log(login("admin", "admin"));
+// -- Tugas 1 --
+const siswa1 = {
+  id: 1,
+  nama: "Budi Santoso",
+  kelas: "XII RPL 1",
+};
+
+console.log(siswa1);
+
+// -- Tugas 2 --
+function hitungUmur(tahunLahir) {
+  const tahunSekarang = new Date().getFullYear();
+  return tahunSekarang - tahunLahir;
+}
+
+const umurSiswa1 = hitungUmur(2005);
+console.log(`Umur ${siswa1.nama}: ${umurSiswa1} tahun`);
